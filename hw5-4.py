6c67394294d76fda11b2c73153ce89697592a780
p22strautz <p22strautz@fairfieldprep.org>
2021-11-02 19:09:51 -0400

    hw5-4

---
+++ hw5-4.py
@@ -0,0 +1,31 @@
+# Author: SCT (AMDG) 11/2/21
+
+# Question 1
+print("Python"[2])
+
+# Question 2
+print("Strings are simply a sequence of characters."[30])
+
+# Question 3
+print(len("excellent"))
+
+# Question 4
+print("apple" in "Pineapple")
+
+# Question 5
+print("Peach" > "pear")
+
+# Question 6
+print("x" * 100)
+
+# Question 7
+print(str(54321))
+
+# Question 8
+print("p" not in "Pineapple")
+
+# Question 9
+print('\"Livin\' On a Prayer\" is my friend\'s favorite song.')
+
+# Question 10
+print(len('\"Livin\' On a Prayer\" is my friend\'s favorite song.'))
